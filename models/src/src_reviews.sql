8ddc42ab0fae8fa024961e4cca8638f87d58a651
extern-jesusarbol <extern.david-jesus.arbol@seat.es>
2026-05-09 21:18:43 +0200

    Primer commit

---
+++ models/src/src_reviews.sql
@@ -0,0 +1,12 @@
+WITH raw_reviews AS(
+    SELECT * FROM {{ source('airbnb', 'reviews') }}
+)
+
+SELECT
+    date AS review_date,
+    comments AS review_text,
+    sentiment AS review_sentiment,
+    listing_id
+    revirer_name
+FROM 
+    raw_reviews
